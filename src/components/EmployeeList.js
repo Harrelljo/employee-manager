c72fbeffeb8edf0309d6a9252a401edb8121ee2d
Harrelljo <jontae.harrell@gmail.com>
2024-12-02 18:10:44 -0800

    Update EmployeeList.js

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,22 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
-function EmployeeList({ employees }) {
+function AddEmployee({ addEmployee }) {
+  const [employee, setEmployee] = useState({
+    name: "",
+    EmployeeId: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEmployee({ ...employee, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (employee.name && employee.EmployeeId) {
+      addEmployee(employee);
+      setEmployee({ name: "", EmployeeId: "" });
+    }
+  };
+
   return (
-    <div className="employee-list">
-      <h2>Employee List</h2>
-      <ul>
-        {employees.map((employee) => (
-          <li key={employee.EmployeeId}>
-            {/* Create a link to the employee detail page */}
-            <Link to={`/employees/${employee.EmployeeId}`}>
-              {employee.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h2>Add Employee</h2>
+      <input
+        type="text"
+        name="name"
+        value={employee.name}
+        onChange={handleChange}
+        placeholder="Employee Name"
+      />
+      <input
+        type="text"
+        name="EmployeeId"
+        value={employee.EmployeeId}
+        onChange={handleChange}
+        placeholder="Employee ID"
+      />
+      <button type="submit">Add Employee</button>
+    </form>
   );
 }
 
-export default EmployeeList;
+export default AddEmployee;
